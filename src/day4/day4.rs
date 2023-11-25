6ba8ca5741bd51f18c56765f41ea7e54d2707eb0
Kushal Talele <kushal.talele@gmail.com>
2023-11-24 21:59:51 -0500

    - day4 done

--- src/day4/day4.rs
+++ src/day4/day4.rs
@@ -1,3 +1,61 @@
-pub fn execute(data: Vec<String>) {
-   dbg!("data");
+fn get_min_and_max(s: &str) -> (i32, i32) {
+    let l = s.split("-").collect::<Vec<&str>>()[0];
+    let r = s.split("-").collect::<Vec<&str>>()[1];
+    return (l.to_string().parse().unwrap(), r.to_string().parse().unwrap());
 }
+pub fn execute(data: Vec<String>) {
+    let result = data.clone().iter()
+        .fold(0, |acc, pairs| {
+
+            let l = pairs.split(",").collect::<Vec<&str>>()[0];
+            let r = pairs.split(",").collect::<Vec<&str>>()[1];
+            
+            let (l_min, l_max) = get_min_and_max(l);
+            let (r_min, r_max) = get_min_and_max(r);
+
+            if r_min >= l_min && r_max <= l_max {
+                return acc + 1;
+            }
+
+            if l_min >= r_min && l_max <= r_max {
+                return acc + 1;
+            }
+            
+            return acc;
+        });
+
+    dbg!(result);
+
+    let result_2 = data.iter()
+        .fold(0, |acc, pairs| {
+
+            let l = pairs.split(",").collect::<Vec<&str>>()[0];
+            let r = pairs.split(",").collect::<Vec<&str>>()[1];
+            
+            let (l_min, l_max) = get_min_and_max(l);
+            let (r_min, r_max) = get_min_and_max(r);
+
+            // check if l_min overlaps
+            if l_min >= r_min && l_min <= r_max {
+                return acc + 1;
+            }            
+            // check if l_max overlaps
+            if l_max >= r_min && l_max <= r_max {
+                return acc + 1;
+            }            
+            // check if r_min overlaps
+            if r_min >= l_min && r_min <= l_max {
+                return acc + 1;
+            }
+            // check if r_max overlaps
+            if r_max >= l_min && r_max <= l_max {
+                return acc + 1;
+            }
+
+            return acc;
+        });
+
+    dbg!(result_2);
+
+}
+
