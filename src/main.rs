d017f83299da37062ced8dccc24b387133c70a93
Kushal Talele <kushal.talele@gmail.com>
2023-11-24 21:39:59 -0500

    - day4 skel

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 //#IMPORTMARKER
 
 /// A fictional versioning CLI
@@ -60,6 +61,7 @@ fn main() {
                 Some(2) => day2::day2::execute(d),
                 Some(3) => day3::day3::execute(d),
                 
+                Some(4) => day4::day4::execute(d),
                 //#CASEMARKER
                 None => todo!(),
                 _ => todo!(),
