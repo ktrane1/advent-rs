b030573fcec66addee2866c7f4967236995d39ec
Kushal Talele <kushal.talele@gmail.com>
2023-11-25 20:59:55 -0500

    - cargo watch - day6 part1

--- src/day6/day6.rs
+++ src/day6/day6.rs
@@ -1,3 +1,18 @@
+use std::collections::HashSet;
+
 pub fn execute(data: Vec<String>) {
-   dbg!("data");
+    let mut result: i32 = 0;
+    let str = data.get(0).unwrap();
+    let mut set: HashSet<&char> = HashSet::new();
+    for (i, elements) in str.chars().collect::<Vec<char>>().windows(4).enumerate() {
+        for letter in elements {
+            set.insert(letter); 
+        }
+        if set.len() == 4 {
+            result = i as i32 + 4;
+            break
+        }
+        set.drain();
+    }
+    dbg!(result);
 }
