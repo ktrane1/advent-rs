807f6172e22ea45824c294949f253b02c8d44bf0
Kushal Talele <kushal.talele@gmail.com>
2023-11-25 20:18:49 -0500

    - formatting - day25 part1

--- src/day25/day25.rs
+++ src/day25/day25.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone)]
 struct SnafuDigit {
     val: Option<String>,
     next: Option<Box<SnafuDigit>>,
@@ -27,7 +28,35 @@ impl SnafuDigit {
     fn print_snaf(&mut self) {
         let mut d: Vec<String> = Vec::new();
 
-        self.val.clone().unwrap();
+        d.push(self.val.clone().unwrap().to_string());
+
+        let mut next = &self.next;
+
+        while next.is_some() {
+            match next.as_ref() {
+                Some(sd) => {
+                    d.push(sd.val.clone().unwrap().to_string());
+                    next = &sd.next;
+                }
+                None => {
+                    next = &None;
+                }
+            };
+        }
+
+        d.reverse();
+
+        let s = d
+            .iter()
+            .fold(String::from(""), |acc, d| format!("{}{}", acc, d));
+
+        dbg!(s);
+    }
+
+    fn print_decimal(&mut self) {
+        let mut d: Vec<String> = Vec::new();
+
+        d.push(self.decimal_value.clone().unwrap().to_string());
 
         let mut next = &self.next;
 
@@ -57,21 +86,23 @@ impl SnafuDigit {
             self.exponent = Some(exponent);
             self.decimal_value = Some(remainder);
             return;
-        }
+        };
 
-        let mut next = &self.next;
+        let n = &mut None::<Box<SnafuDigit>>;
+
+        let mut next = &mut self.next;
         while next.is_some() {
-            match next.as_ref() {
+            match next {
                 Some(sd) => {
-                    next = &sd.next;
+                    next = &mut sd.next;
                 }
-                None => {
-                    next = &None;
+                _ => {
+                    next = n;
                 }
             };
         }
 
-        next = &Some(Box::new(SnafuDigit::new_with_values(exponent, remainder)));
+        let _ = next.insert(Box::new(SnafuDigit::new_with_values(exponent, remainder)));
     }
 
     pub fn is_empty(&mut self) -> bool {
@@ -83,6 +114,63 @@ impl SnafuDigit {
             return true;
         }
         return false;
+    }
+
+    // make decimal values to snaf digits
+    pub fn encode(&mut self) {
+        process(self);
+
+        if self.next.is_none() {
+            return;
+        }
+
+        let mut next = self.next.as_deref_mut().unwrap();
+
+        process(next);
+
+        while next.next.is_some() {
+            next = next.next.as_deref_mut().unwrap();
+            process(next);
+        }
+    }
+
+    pub fn inc_next(&mut self) {
+        if self.next.as_deref_mut().is_none() {
+            self.push(self.exponent.unwrap() + 1, 1);
+        } else {
+            match self.next.as_deref_mut().unwrap().decimal_value {
+                Some(n) => {
+                    self.next.as_deref_mut().unwrap().decimal_value = Some(n + 1);
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+fn process(snafu_digit: &mut SnafuDigit) {
+    match snafu_digit.decimal_value {
+        Some(num) => match num {
+            n if n < 3 => {
+                snafu_digit.val = Some(n.to_string());
+            }
+            3 => {
+                snafu_digit.val = Some("=".to_string());
+                snafu_digit.inc_next();
+            }
+            4 => {
+                snafu_digit.val = Some("-".to_string());
+                snafu_digit.inc_next();
+            }
+            n if n > 4 => {
+                snafu_digit.val = Some("0".to_string());
+                snafu_digit.inc_next();
+            }
+            unknown => panic!("digit out of range: {:?}", unknown),
+        },
+        None => {
+            dbg!("dec value is none");
+        }
     }
 }
 
@@ -113,34 +201,31 @@ fn int_to_snafu(decimal: i64) {
     const BASE: i64 = 5;
     let mut exponent: i32 = 0;
     let mut current = decimal / BASE;
+    let mut remainder = decimal % BASE;
     let mut snaf = SnafuDigit::new();
 
-    loop {
-        let remainder = current % BASE;
-
+    while current > 0 || remainder > 0 {
         snaf.push(
             exponent,
             remainder.try_into().expect("could not coerce to i32"),
         );
 
+        remainder = current % BASE;
         current = current / BASE;
         exponent += 1;
-        if current == 0 {
-            break;
-        }
     }
 
+    snaf.print_decimal();
+    snaf.encode();
     snaf.print_snaf();
 }
 
 pub fn execute(data: Vec<String>) {
     let result = data.iter().fold(0, |acc, snaf| {
-        dbg!(acc.clone());
         return acc + parse(snaf.to_string());
     });
-    dbg!(result);
 
-    int_to_snafu();
+    int_to_snafu(result);
 }
 
 // SNAFU
@@ -151,3 +236,6 @@ pub fn execute(data: Vec<String>) {
 // 0 = 0
 // = = -2
 // - = -1
+//
+// 2=01 = 1301
+// 1-12 = 412
