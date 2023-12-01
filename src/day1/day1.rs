1d1396f39f97c6b0bc9fa9cefb95766921c6fecf
ktalele-rescale <ktalele+github@rescale.com>
2023-12-01 16:37:36 -0500

    - day1 p1

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -1,3 +1,33 @@
 pub fn execute(data: Vec<String>) {
-   dbg!("data");
+    let result_part1 = data.iter()
+        .map(|line| {
+            let mut first: char = char::default();
+            let mut second: char = char::default();
+
+            for x in line.chars() {
+                match x.to_digit(10) {
+                    Some(num) => {
+                        first = x;
+                        break;
+                    },
+                    None => {},
+                }
+            }
+
+            for x in line.chars().rev() {
+                match x.to_digit(10) {
+                    Some(num) => {
+                        second = x;
+                        break;
+                    },
+                    None => {},
+                }
+            }
+
+            return vec![first, second].into_iter().collect::<String>();
+        })
+    .map(|s| s.parse::<usize>().unwrap())
+        .sum::<usize>();
+
+    dbg!(result_part1);
 }
