081bdb10fdfdb2b4d371d601964928e56d77ef0a
ktalele-rescale <ktalele+github@rescale.com>
2023-12-01 16:19:27 -0500

    2023setup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 use clap::{Args, Parser, Subcommand};
 use std::fs;
 
-mod day1;
-mod day2;
-mod day25;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
 //#IMPORTMARKER
 
 /// A fictional versioning CLI
@@ -60,13 +53,6 @@ fn main() {
                 .collect();
 
             match exec.day {
-                Some(1) => day1::day1::execute(d),
-                Some(2) => day2::day2::execute(d),
-                Some(3) => day3::day3::execute(d),
-                Some(4) => day4::day4::execute(d),
-                Some(5) => day5::day5::execute(d),
-                Some(25) => day25::day25::execute(d),
-                Some(6) => day6::day6::execute(d),
                 //#CASEMARKER
                 None => todo!(),
                 _ => todo!(),
